5112dceb7ca33c18aed18809491574f4e9e0af40
aassdd236 <aassdd236@naver.com>
2024-01-11 12:13:29 +0900

    equals 설명

--- src/com/javaex/ex03/PointApp.java
+++ src/com/javaex/ex03/PointApp.java
@@ -9,11 +9,11 @@ public class PointApp {
 		Point p02=new Point(13, 15);
 		Point p03=p02;
 		
-		System.out.println(p00.equals(p01));
-		System.out.println(p00==p01);
+		System.out.println(p00.equals(p01)); //값이 같으면 true, 아니면 false
+		System.out.println(p00==p01); //주소 비교 => 주소가 같으면 true, 아니면 false(값이 같아도 주소가 다르면 false)
 		
-		System.out.println(p01.equals(p02));
-		System.out.println(p03.equals(p02));
+		System.out.println(p01.equals(p02)); //논리 비교
+		System.out.println(p03.equals(p02)); //논리 비교
 	}
 
 }
